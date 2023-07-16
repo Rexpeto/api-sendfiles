3b8b66a46e5970357f5ad70bce6f296b5b9d0eef
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-07-16 14:57:15 -0400

    Connect to mongoDB

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,17 @@
 import "dotenv/config";
 import express from "express";
 import cors from "cors";
+import dbConnect from "./config/mongodb";
 
 const app = express();
 const PORT = process.env.PORT ?? 4000;
 
 app.use(cors());
 app.use(express.json());
+
+//? Connection to mongo
+dbConnect()
+    .then(() => console.log(`🔌 Connect to mongo`))
+    .catch(() => console.log("❌ Problem to connect mongoDB"));
 
 app.listen(PORT, () => console.log(`⚡ Working by the port ${PORT}`));
