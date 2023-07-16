192e939278dd5cfeda63a3ae53f403d78c656c24
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-07-16 19:55:32 -0400

    Verified with bcrypt

--- src/utils/bcrypt.handle.ts
+++ src/utils/bcrypt.handle.ts
@@ -5,3 +5,9 @@ export const encrypt = (password: string) => {
 	const passwordHash = hash(password, 10);
 	return passwordHash;
 };
+
+//? Verified password of user
+export const Verified = async (passwordHash: string, password: string) => {
+	const isCorrect = await compare(password, passwordHash);
+	return isCorrect;
+};
