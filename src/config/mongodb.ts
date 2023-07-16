2acf0e8f43fc798610187899b848eb692a7b2f52
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-07-16 14:18:16 -0400

    Connection to mongoDB

---
+++ src/config/mongodb.ts
@@ -0,0 +1,8 @@
+import { connect } from "mongoose";
+
+const dbConnect = async () => {
+    const DB_URI: string = process.env.DB_URI ?? "mongodb://localhost:27017/sendfile-api";
+    await connect(DB_URI);
+};
+
+export default dbConnect;
