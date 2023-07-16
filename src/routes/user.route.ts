e7453b274cd9b9db4b8779596d947f1a84cbbafc
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-07-16 17:05:55 -0400

    Route login and register user

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,3 +1,10 @@
 import { Router } from "express";
+import { login, register } from "../controllers/user.ctrl";
 
 export const router = Router();
+
+//? Route of login user
+router.post("/login", login);
+
+//? Route of register user
+router.post("/register", register);
