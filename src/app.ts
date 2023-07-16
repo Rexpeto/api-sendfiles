f83c8a18d92094f03c8f6346461462c585f1a658
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-07-16 15:46:48 -0400

    Use dynamic routes

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,14 @@ import "dotenv/config";
 import express from "express";
 import cors from "cors";
 import dbConnect from "./config/mongodb";
+import router from "./routes";
 
 const app = express();
 const PORT = process.env.PORT ?? 4000;
 
 app.use(cors());
 app.use(express.json());
+app.use(router);
 
 //? Connection to mongo
 dbConnect()
