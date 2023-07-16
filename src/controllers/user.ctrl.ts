9deb9805e760dd67061b49cb3dd6c449429be018
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-07-16 19:51:01 -0400

    Register user

--- src/controllers/user.ctrl.ts
+++ src/controllers/user.ctrl.ts
@@ -1,9 +1,28 @@
 import { Request, Response } from "express";
+import { newUser } from "../services/user.service";
 
 export const login = (req: Request, res: Response) => {
-    res.status(200).json({ msg: "Iniciar sesión" });
+	res.status(200).json({ msg: "Iniciar sesión" });
 };
 
-export const register = (req: Request, res: Response) => {
-    res.status(200).json({ msg: "registrar usuario" });
+export const register = async ({ body }: Request, res: Response) => {
+	const { name, email, password } = body;
+
+	if (!name) return res.status(403).json({ msg: "Debe colocar un nombre" });
+
+	if (!email) return res.status(403).json({ msg: "Debe colocar un correo electronico" });
+
+	if (!password) return res.status(403).json({ msg: "Debe colocar una contraseña" });
+
+	const user: any = await newUser({
+		name: name.toLowerCase(),
+		email: email.toLowerCase(),
+		password: password.toLowerCase()
+	});
+
+	if (user?.error) {
+		return res.status(403).json({ msg: "El usuario ya existe" });
+	}
+
+	res.status(200).json(user);
 };
