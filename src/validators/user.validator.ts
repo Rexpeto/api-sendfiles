91fbfa4fe4ff335e8b074eb2aa1e14f08b69a3ad
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-07-17 21:39:25 -0400

    Validater register user

---
+++ src/validators/user.validator.ts
@@ -0,0 +1,12 @@
+import { check } from "express-validator";
+import { validateResult } from "../utils/validator.handle";
+import { NextFunction, Request, Response } from "express";
+
+export const validateRegister = [
+	check("name", "Debe colocar un nombre").exists().notEmpty(),
+	check("email", "Debe colocar un correo eléctronico").exists().isEmail().withMessage("Correo eléctronico inválido"),
+	check("password", "Debe colocar una contraseña").exists().notEmpty(),
+	(req: Request, res: Response, next: NextFunction) => {
+		validateResult(req, res, next);
+	}
+];
